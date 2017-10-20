5feed61b3839e7b6606c06dae38f4f421ffdda3f
Mateusz Szymczak <mateuszszymczak15@gmail.com>
2017-10-20 21:44:20 +0200

    Added Game LocalDb context and user entity with simple data.

---
+++ GameApplication/GameApplication/Data/DbInitializer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GameApplication.Models;
+
+namespace GameApplication.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(GameContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Users.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            var users = new User[]
+                {
+                    new User{Username="user1",Password="pass1"},
+                    new User{Username="user2",Password="pass2"},
+                    new User{Username="user3",Password="pass3",},
+                    new User{Username="maciej",Password="rutkowski"}
+                };
+
+            foreach (User user in users)
+            {
+                context.Users.Add(user);
+            }
+            context.SaveChanges();
+        }
+    }
+}
---
+++ GameApplication/GameApplication/Data/GameContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GameApplication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameApplication.Data
+{
+    public class GameContext : DbContext
+    {
+        public GameContext(DbContextOptions<GameContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("User");
+        }
+    }
+}
