05c46f687bf39945f307a49e364a3a4781908d48
adzka95 <ada.rynkowska@wp.pl>
2017-10-23 17:29:14 +0200

    Split template to part. Add left and top menu to shared code.

--- GameApplication/GameApplication/Controllers/HomeController.cs
+++ GameApplication/GameApplication/Controllers/HomeController.cs
@@ -33,5 +33,6 @@ namespace GameApplication.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
--- GameApplication/GameApplication/Startup.cs
+++ GameApplication/GameApplication/Startup.cs
@@ -43,7 +43,7 @@ namespace GameApplication
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Template}/{action=Index}/{id?}");
             });
         }
     }
