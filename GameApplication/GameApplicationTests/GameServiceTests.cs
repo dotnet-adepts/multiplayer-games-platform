ad01b12bad08064bd48fbcc2ba23db1ffc6335b1
Mateusz Szymczak <mateuszszymczak15@gmail.com>
2017-12-03 18:21:09 +0100

    Added tests for GameService and Lobby

---
+++ GameApplication/GameApplicationTests/GameServiceTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GameApplication.Factories;
+using GameApplication.Models.Games;
+using GameApplication.Services;
+using Xunit;
+
+namespace GameApplicationTests
+{
+
+    public class GameServiceTests
+    {
+        private GameService _gameService;
+
+        public GameServiceTests()
+        {
+            _gameService = new GameService();
+        }
+
+        [Fact]
+        public void ShouldFindAllGamesNames()
+        {
+            //Arrange
+            List<string> games = new List<string>
+            {
+                Game.Names.Snake,
+                Game.Names.Battleship
+            };
+
+            //Act
+            var gamesNames = _gameService.FindAllGamesNames();
+
+            //Assert
+            Assert.Equal(gamesNames, games);
+        }
+
+        [Fact]
+        public void ShouldFindByGameName()
+        {
+            //Arrange
+            var game = new Game(Game.Names.Battleship, "dluga nazwa", 2, 2, "Strategiczne", new BatlleshipGameSessionFactory());
+
+            //Act
+            var battleshipGame = _gameService.FindByGameName(Game.Names.Battleship);
+
+            //Assert
+            Assert.NotNull(battleshipGame);
+            Assert.Equal(battleshipGame.Category, game.Category);
+            Assert.Equal(battleshipGame.MaxNumberOfPlayers, game.MaxNumberOfPlayers);
+            Assert.Equal(battleshipGame.Name, game.Name);
+        }
+    }
+}
---
+++ GameApplication/GameApplicationTests/LobbyServiceTests.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using GameApplication.Factories;
+using GameApplication.Models.Games;
+using GameApplication.Services;
+using Moq;
+using Xunit;
+
+namespace GameApplicationTests
+{
+    public class LobbyServiceTests
+    {
+
+        private LobbyService _lobbyService;
+
+        private Game _snake;
+
+        private Game _battleships;
+
+        public LobbyServiceTests()
+        {
+            _lobbyService = new LobbyService(new GameService());
+            _snake = new Game(Game.Names.Snake, "Steruj wê¿em, pokonaj innych!", 2, 4, "Zrêcznoœciowe", null);
+            _battleships = new Game(Game.Names.Battleship, "Zatop statki przeciwnika zanim on zatopi Twoje!", 2, 2, "Strategiczne", new BatlleshipGameSessionFactory());
+        }
+
+        [Fact]
+        public void ShouldCreateBattleshipLobby()
+        {
+            //Arrange
+            var mockedClaims = new Mock<ClaimsPrincipal>();
+            Player player = new Player(mockedClaims.Object);
+            Lobby lobby = new Lobby(_battleships, player);
+
+            //Act
+            Lobby receivedLobby = _lobbyService.Create(Game.Names.Battleship, player);
+
+            //Assert
+            Assert.NotNull(receivedLobby);
+            Assert.Equal(lobby.Game.Category, receivedLobby.Game.Category);
+            Assert.Equal(lobby.Game.Name, receivedLobby.Game.Name);
+            Assert.Equal(lobby.Id + 1, receivedLobby.Id);
+
+        }
+
+        [Fact]
+        public void ShouldCreateSnakeLobby()
+        {
+            //Arrange
+            var mockedClaims = new Mock<ClaimsPrincipal>();
+            Player player = new Player(mockedClaims.Object);
+            Lobby lobby = new Lobby(_snake, player);
+
+            //Act
+            Lobby receivedLobby = _lobbyService.Create(Game.Names.Snake, player);
+
+            //Assert
+            Assert.NotNull(receivedLobby);
+            Assert.Equal(lobby.Game.Category, receivedLobby.Game.Category);
+            Assert.Equal(lobby.Game.Name, receivedLobby.Game.Name);
+            Assert.Equal(lobby.Id + 1, receivedLobby.Id);
+
+        }
+
+        [Fact]
+        public void ShouldFindEmptyLobbyByGameName()
+        {
+            //Act
+            var lobbies = _lobbyService.FindAllByGameName(Game.Names.Snake);
+
+            //Assert
+            Assert.Empty(lobbies);
+        }
+    }
+}
